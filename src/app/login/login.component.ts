ec5befa12a64f391de11c5e30357d3245f226312
thiampo <babacathiam@gmail.com>
2021-04-21 09:18:00 +0000

    Page de connexion

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+//import { AuthServiceService } from '../auth-service.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+}
